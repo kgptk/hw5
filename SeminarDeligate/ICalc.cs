ed76ea86468401fdc0fe8d1f795630d9d7b561c3
alzavrmk <3845303@gmail.com>
2023-11-21 21:15:30 +0300

    addI ICalc.cs

---
+++ SeminarDeligate/ICalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarDeligate
+{
+    public interface ICalc
+    {
+        void Sum(int x);
+        void Sub(int x);
+        void Mul(int x);
+        void Divide(int x);
+        void CanselLast();
+
+        event EventHandler<EventArgs> MyEventHandler;
+    }
+}
