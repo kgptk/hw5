3b8698211556e0984375af2ca48952d99c49427c
alzavrmk <3845303@gmail.com>
2023-11-23 12:28:06 +0300

    Add modif NegativeExeption.cs

---
+++ SeminarDeligate/NegativeExeption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarDeligate
+{
+    public class NegativeExeption: Exception
+    {
+        NegativeExeption()
+        {
+
+        }
+        public NegativeExeption(string message): base(message) 
+        {
+
+        }
+
+        public NegativeExeption(string message, Exception ex) : base(message, ex)
+        {
+
+        }
+
+
+    }
+}
